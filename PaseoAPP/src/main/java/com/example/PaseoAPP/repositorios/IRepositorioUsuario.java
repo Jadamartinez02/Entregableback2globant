f70bc906b94f1918d10e2002ccf73fdd20807fc2
Javier David Martinez Arrieta <javier.martinez02@globant.com>
2026-04-07 11:41:32 -0500

    Lo que se trabajo en clase

--- PaseoAPP/src/main/java/com/example/PaseoAPP/repositorios/IRepositorioUsuario.java
+++ PaseoAPP/src/main/java/com/example/PaseoAPP/repositorios/IRepositorioUsuario.java
@@ -4,8 +4,10 @@ import com.example.PaseoAPP.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IRepositorioUsuario extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByCorreo(String correo);
 }
--- PaseoAPP/src/main/java/com/example/PaseoAPP/servicios/UsuarioServicio.java
+++ PaseoAPP/src/main/java/com/example/PaseoAPP/servicios/UsuarioServicio.java
@@ -3,7 +3,9 @@ package com.example.PaseoAPP.servicios;
 import com.example.PaseoAPP.modelos.Usuario;
 import com.example.PaseoAPP.repositorios.IRepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -17,6 +19,10 @@ public class UsuarioServicio {
     public boolean guardarUsuarioEnBD(Usuario datos){
         //validar que datos me envian y si estos cumplen las reglas del negocio
         //Guardar los datos en BD con ayuda del repositorio
+        //Validar que el correo a registrar no se haya guardado previamente
+        if(repositorioUsuario.findByCorreo(datos.getCorreo()).isPresent()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "El correo "+ datos.getCorreo() + " ya existe");
+        }
         return false;
     }
     public boolean modificarUsuarioEnBD(Usuario datos, UUID id){
